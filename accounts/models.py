ee3bf1688ef86bfc27e2dadbff8392e17ef689fb
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2021-01-29 12:53:35 +0300

    Чот перемудрил с 1-2-1 юзера на профиль...

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ class Student(models.Model):
     firstname = models.CharField(max_length=30, blank=False, null=False)
     lastname = models.CharField(max_length=30, blank=False, null=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(blank=True, null=True, default=True)
 
     def get_absolute_url(self):
         return reverse('student-detail', kwargs={'pk': self.pk})
@@ -19,6 +20,7 @@ class Teacher(models.Model):
     firstname = models.CharField(max_length=30, blank=False, null=False)
     lastname = models.CharField(max_length=30, blank=False, null=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(blank=True, null=True, default=True)
 
     def get_absolute_url(self):
         return reverse('teacher-detail', kwargs={'pk': self.pk})
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,74 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView
+
+from .forms import LoginForm, UserRegistrationForm
 from .models import Teacher, Student
 
-class TeacherCreate(CreateView):
-    model = Teacher
-    fields = '__all__'
-    success_url = reverse_lazy('courses:index')
 
 
-class StudentCreate(CreateView):
-    model = Student
-    fields = '__all__'
-    success_url = reverse_lazy('courses:index')
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                            username=cd['username'],
+                            password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
+
+def register_teacher(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            Teacher.objects.create(user=new_user)
+            return render(request,
+                          'accounts/signup_complete.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request,
+                  'accounts/teacher_form.html',
+                  {'user_form': user_form})
+
+
+def register_student(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            Student.objects.create(user=new_user)
+            return render(request,
+                          'accounts/signup_complete.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request,
+                  'accounts/student_form.html',
+                  {'user_form': user_form})
 
 class TeacherDetails(DetailView):
     model = Teacher

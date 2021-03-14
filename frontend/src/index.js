15463a32981eb0cadc102b81a5d2ee3f01075589
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2021-03-14 22:21:34 +0300

    First axios response

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,12 @@
-alert(1);
+import axios from "axios";
+
+const url = 'http://127.0.0.1:8000/api/courses/';
+
+function getCoursesInfo(cb) {
+  axios ({
+    method: 'get',
+    url
+  }).then((response)=> cb(response.data));
+}
+
+getCoursesInfo(console.log.bind(console, 'getCoursesInfo'));

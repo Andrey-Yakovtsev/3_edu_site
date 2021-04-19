19e9aaa75b8041c58764da25bcaa4d5618c94d95
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2021-04-19 10:35:39 +0300

    Переделал на хуки. прикрутил навбар линки Навесил

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,10 @@ import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import {Link, BrowserRouter as Router, Switch} from "react-router-dom";
 
 function App() {
-    const CourseItem = ({item}) => {
-    return (
-        <tr>
-            <td>{item.id}</td>
-            <td>{item.title}</td>
-            <td>{item.description}</td>
-            <td>{item.start_date}</td>
-        </tr>
-    )
-}
+
     const [courses, setCourses] = useState([])
 
     useEffect(() => {
@@ -30,43 +21,20 @@ function App() {
         <div className='container'>
             <Header title = 'These are our courses' />
             <hr/>
-            <ul>
-                {courses.map(course => (
-                    <li key={course.id}>
-                        {course.title}
-                    </li>
-                ))}
-            </ul>
+            <Router>
+                <ul>
+                    {courses.map(course => (
+                        <li>
+                            <Link to={{pathname: `/course/${course.id}/`, fromDashboard: false}}>
+                                {course.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </Router>
         </div>
     );
 }
 export default App;
 
-//
-//
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             courses: [],
-//         }
-//     }
-//
-//
-//
-//     render() {
-//         return(
-//             <div className={App}>
-//                 <button onClick={() => this.getCoursesInfo()}>
-//                     Получить список курсов
-//                 </button>
-//                 <CourseList items={this.state.courses} />
-//             </div>
-//         );
-//     }
-//
-//     componentDidUpdate() {
-//         console.log('Перерисовался успешно. Можно что-то поделать ТУТ!!!')
-//     }
-// }
-// export default App;
+
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,52 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 const Header = () => {
 
+    const [courses, setCourses]  = useState([])
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'http://127.0.0.1:8000/api/courses/'
+        }).then(response => {
+            setCourses(response.data)
+        })
+    }, [])
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                         aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">Features</a>
-                        <a className="nav-link" href="#">Pricing</a>
-                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                    </div>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                               data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown link
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                {courses.map(course =>
+                                <li><a className="dropdown-item" href="#" key={course.id}>{course.title}</a></li>
+                                )}
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
